cd56ebb87a2851c2bdfe1c7c7324c79502fe5670
terkel89 <terkel89@gmail.com>
2015-11-29 16:51:01 +0200

    Вирішив квадратне рівняння через Case

---
+++ script.js
@@ -0,0 +1,48 @@
+
+var a, b, c, c, D, x, x1, x2;
+
+
+a = -4;
+b = 3;
+c = 1
+console.log("Медот через if")
+D = b*b-4*a*c;
+
+if (D == 0) {
+	x = -b/(2*a)
+	console.log(x);
+}
+
+else if (D > 0) {
+	x1 = (-b+Math.sqrt(D))/(2*a);
+	x2 = (-b-Math.sqrt(D))/(2*a);
+	console.log(x1);
+	console.log(x2);
+}
+
+else {
+	x = "Error";
+console.log(x);
+}
+console.log("Медот через case")
+D = b*b-4*a*c;
+D = D / D;
+switch (D) {
+	case -1 :
+		x = "Error";
+		console.log(x);
+	break;
+	case 1 :
+		D = b*b-4*a*c;
+		x1 = (-b+Math.sqrt(D))/(2*a);
+		x2 = (-b-Math.sqrt(D))/(2*a);
+		console.log(x1);
+		console.log(x2);
+	break;
+	default :
+		D = b*b-4*a*c;
+		x = -b/(2*a)
+		console.log(x);
+		
+	break;
+}
